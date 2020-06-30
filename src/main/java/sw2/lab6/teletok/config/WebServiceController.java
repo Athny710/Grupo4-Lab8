821310b9f9c93f339e6f2384bf1bc66e1db5358c
Julio Carrion <a20161448@pucp.edu.pe>
2020-06-29 20:02:18 -0500

    anthony te odio2

--- src/main/java/sw2/lab6/teletok/config/WebServiceController.java
+++ src/main/java/sw2/lab6/teletok/config/WebServiceController.java
@@ -9,8 +9,10 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import sw2.lab6.teletok.entity.Post;
+import sw2.lab6.teletok.entity.PostComment;
 import sw2.lab6.teletok.entity.StorageServices;
 import sw2.lab6.teletok.entity.Token;
+import sw2.lab6.teletok.repository.PostCommentRepository;
 import sw2.lab6.teletok.repository.PostRepository;
 import sw2.lab6.teletok.repository.TokenRepository;
 
@@ -21,26 +23,30 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin
+@RequestMapping ("/ws")
 public class WebServiceController {
 
     @Autowired
     TokenRepository tokenRepository;
     @Autowired
     PostRepository postRepository;
+    @Autowired
+    PostCommentRepository postCommentRepository;
 
-    @PostMapping(value = "/product", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity GuardarPost(
-            @RequestBody String token, @RequestBody String description, @RequestBody MultipartFile file) {
+    @PostMapping(value = "/post/save", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity crearPost(
+            @RequestBody String token, @RequestBody String description, @RequestBody MultipartFile media) {
 
         HashMap<String, Object> responseMap = new HashMap<>();
         StorageServices storageServices = new StorageServices();
-        HashMap<String, String> map = storageServices.store(file);
+        HashMap<String, String> map = storageServices.store(media);
         Optional<Token> optionalToken = tokenRepository.findByCode(token);
         if(optionalToken.isPresent()){
             if(map.get("estado").equalsIgnoreCase("exito")){
                  Post post=new Post();
                  post.setMediaUrl(map.get("fileName"));
                  post.setDescription(description);
+                 post.setUser(optionalToken.get().getUser());
                  postRepository.save(post);
                 List<Post> posts = postRepository.findAll();
                 responseMap.put("postId", posts.get(posts.size()-1));
@@ -51,11 +57,35 @@ public class WebServiceController {
         }else{
             responseMap.put("error", "TOKEN_INVALID");
         }
-
-
         return new ResponseEntity(responseMap, HttpStatus.CREATED);
-
     }
+
+    @PostMapping(value = "/post/comment", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity GuardarComentario(
+                @RequestBody String token, @RequestBody int postId, @RequestBody String message) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+        Optional<Token> optionalToken = tokenRepository.findByCode(token);
+        if(optionalToken.isPresent()){
+            Optional<Post> postOptional = postRepository.findById(postId);
+            if(postOptional.isPresent()){
+                PostComment postComment = new PostComment();
+                postComment.setMessage(message);
+                postComment.setPost(postOptional.get());
+                postComment.setUser(optionalToken.get().getUser());
+                List<PostComment> postComments = postCommentRepository.findAll();
+                responseMap.put("commentId", postComments.get(postComments.size()-1));
+                responseMap.put("status", "COMMENT_CREATED");
+            }else {
+                responseMap.put("error", "POST_NOT_FOUND");
+            }
+        }else{
+            responseMap.put("error", "TOKEN_INVALID");
+        }
+        return new ResponseEntity(responseMap, HttpStatus.CREATED);
+    }
+
+
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity gestionExcepcion(HttpServletRequest request) {

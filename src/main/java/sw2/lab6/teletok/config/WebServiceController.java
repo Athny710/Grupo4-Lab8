886a0390583764b1ee992dc5e5532f1fcb0d5969
Anthony Rojas <a20145976@pucp.pe>
2020-06-29 19:06:54 -0500

    controller

---
+++ src/main/java/sw2/lab6/teletok/config/WebServiceController.java
@@ -0,0 +1,10 @@
+package sw2.lab6.teletok.config;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class WebServiceController {
+
+}
